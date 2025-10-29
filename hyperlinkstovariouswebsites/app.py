dde3c60e2cd829d6cf1f393c3559f195d2b40049
brencerego <brencerego18@gmail.com>
2025-10-29 20:53:19 +0530

    Add files via upload

---
+++ hyperlinkstovariouswebsites/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/social')
+def social_links():
+    links = {
+        "Facebook": "https://www.facebook.com", 
+        "Twitter": "https://www.twitter.com", 
+        "Instagram": "https://www.instagram.com", 
+        "LinkedIn": "https://www.linkedin.com", 
+        "YouTube": "https://www.youtube.com"
+    }
+    return render_template("social.html", links=links)
+
+if __name__ == '__main__':
+    app.run(debug=True)
